12284ec9aaf87ce3bf96f92e93db9e092d6dbf7d
TiannaBassaragh <tianna.bassaragh@outlook.com>
2025-11-23 23:52:19 -0500

    Completed project 1

--- scripts.js
+++ scripts.js
@@ -1,1 +1,25 @@
 /* Add your JavaScript to this file */
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const form = document.querySelector(".newsletter form");
+    const input = document.querySelector("#email");
+    const message = document.querySelector(".message");
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        const email = input.value.trim();
+
+        if (email === "") {
+            message.textContent = "Please enter a valid email address.";
+            return;
+        }
+
+        message.textContent =
+            `Thank you! Your email address ${email} has been added to our mailing list!`;
+
+        input.value = "";
+    });
+
+});
